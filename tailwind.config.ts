0cb0729467680ef55ea2a23d86d11f5285d22acb
Vinay Kumar Reddy <vinay2002116@gmail.com>
2024-12-03 04:07:39 +0530

    added tailwind config and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,35 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "3rem",
+          xl: "4rem",
+          "2xl": "4rem",
+          "3xl": "5rem",
+        },
+      },
+
+      extend: {
+        screens: {
+          "4k": "1980px",
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+}
